cdbac8beadca2fe2d73e4e146afa11d1c37f75f8
Sam Ringer <samr@speechmatics.com>
2019-01-03 09:51:09 +0000

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+echo "source $HOME/git/dotfiles/zsh/zshrc.sh" > $HOME/.zshrc
+echo "source $HOME/git/dotfiles/vim/vimrc.vim" > $HOME/.vimrc
+echo "source $HOME/git/dotfiles/tmux/tmux.conf" > $HOME/.tmux.conf
