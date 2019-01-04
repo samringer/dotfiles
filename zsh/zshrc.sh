ad50077ab311bcde83691fa3a2ff2cfa5e611d8e
Sam Ringer <samr@speechmatics.com>
2019-01-04 17:03:48 +0000

    better substring history searching

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -148,17 +148,20 @@ bindkey -v
 
 # Fix for arrow-key searching
 # start typing + [Up-Arrow] - fuzzy find history forward
-if [[ "${terminfo[kcuu1]}" != "" ]]; then
-	autoload -U up-line-or-beginning-search
-	zle -N up-line-or-beginning-search
-	bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
-fi
+#if [[ "${terminfo[kcuu1]}" != "" ]]; then
+#	autoload -U up-line-or-beginning-search
+#	zle -N up-line-or-beginning-search
+#	bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+#fi
 # start typing + [Down-Arrow] - fuzzy find history backward
-if [[ "${terminfo[kcud1]}" != "" ]]; then
-	autoload -U down-line-or-beginning-search
-	zle -N down-line-or-beginning-search
-	bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
-fi
+#if [[ "${terminfo[kcud1]}" != "" ]]; then
+#	autoload -U down-line-or-beginning-search
+#	zle -N down-line-or-beginning-search
+#	bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+#fi
+
 
 source ~/git/dotfiles/zsh/keybindings.sh
 export PATH=$PATH:$HOME/git/dotfiles/utils
+bindkey "^[[A" history-substring-search-up
+bindkey "^[[B" history-substring-search-down
