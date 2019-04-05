a9cb2c56c49cded19c185ac7fa605ced1c5652c3
Sam Ringer <samr@speechmatics.com>
2019-04-05 17:09:32 +0100

    Allow for slimv in setup

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ sudo apt install tmux
 sudo apt install vim
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+#git clone https://github.com/kovisoft/slimv.git $HOME/.vim/bundle/slimv.vim
 sh ./deploy.sh
