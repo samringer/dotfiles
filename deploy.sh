d073935b0ad2fffe46d872ee1cd18081b71fee86
Sam Ringer <samr@speechmatics.com>
2019-05-28 10:06:21 +0100

    Include some configs

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ echo "source $HOME/git/dotfiles/zsh/zshrc.sh" > $HOME/.zshrc
 echo "source $HOME/git/dotfiles/vim/vimrc.vim" > $HOME/.vimrc
 echo "source $HOME/git/dotfiles/tmux/tmux.conf" > $HOME/.tmux.conf
 echo "[include]" > $HOME/.gitconfig
-echo "path = $HOME/git/dotfiles/git.config" >> $HOME/.gitconfig
+echo "path = $HOME/git/dotfiles/configs/git.config" >> $HOME/.gitconfig
 cat vim/flake8.ignores >> $HOME/.config/flake8
 zsh
