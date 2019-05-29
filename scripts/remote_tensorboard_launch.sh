3b359a3be915b620d71673c35575bd8fded4446c
sam <ringer.sam93@gmail.com>
2019-05-29 19:04:40 +0100

    Update remote tb script

--- scripts/remote_tensorboard_launch.sh
+++ scripts/remote_tensorboard_launch.sh
@@ -1,10 +1,12 @@
 #! /bin/bash -e
 
-logdir=${1:-$PWD}
-
-free_port=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
-nohup bash -c "source ~/git/metalearning/venv/bin/activate && tensorboard --logdir=$logdir --host=localhost --port $free_port" &
-sleep 1
-echo "sshforward $(hostname -f) ${free_port}"
-echo "tensorboard running..."
-exit 0
+port=6006
+# Override port if it is given
+if [[ $# -eq 1 ]]; then
+    port=$1
+fi
+if [[ ! $HOME == "/Users/sam" ]]; then
+    source $HOME/venv/bin/activate
+fi
+echo "tensorboard running on port ${1}"
+tensorboard --logdir=.
--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -168,7 +168,7 @@ alias g="git"
 alias data="cd /home/sam/data"
 alias ns="nvidia-smi"
 alias tb="$HOME/git/dotfiles/scripts/local_tensorboard_launch.sh"
-alias ltb="source $HOME/venv/bin/activate && tensorboard --logdir=."
+alias ltb="$HOME/git/dotfiles/scripts/remote_tensorboard_launch.sh"
 alias todo="tmux attach -t TODO"
 
 # ls after every cd
